36eea06de6e4dce051c6dcc430f4025634407d34
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-17 23:04:04 -0300

    :rocket: styled-components e theme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View } from "react-native";
 import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
@@ -14,5 +15,5 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <View></View>;
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
