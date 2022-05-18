557903ed807492b93b3809a287787c5f1068e157
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-18 00:45:31 -0300

    :rocket: Estrutura do SignIn

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import theme from "./src/theme";
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
+import { SignIn } from "@screens/SignIn";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
@@ -15,5 +17,9 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  );
 }
