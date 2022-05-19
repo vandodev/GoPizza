8027de159f93ba78aab95e9496375a21bd017a22
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-18 22:46:40 -0300

    :rocket: Criando component input utilizando props

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { Container } from "./styles";
 
+import { Input } from "@components/input";
+
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Input
+        placeholder="Email"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none" //Não coloca primeira letra maiúscula
+      />
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+    </Container>
+  );
 }
