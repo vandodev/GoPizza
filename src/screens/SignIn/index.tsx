08edbba9292d8bfeb15d4846a200cf3c2c12f841
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-19 21:33:34 -0300

    :rocket: KeyboardAvoidingView e react-native-iphone-x-helper

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container } from "./styles";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { Container, Content } from "./styles";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
@@ -7,14 +8,20 @@ import { Button } from "@components/Button";
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="Email"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none" //Não coloca primeira letra maiúscula
-      />
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
-      <Button title={"Entrar"} type={"secondary"} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Input
+            placeholder="Email"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none" //Não coloca primeira letra maiúscula
+          />
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <Button title={"Entrar"} type={"secondary"} />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 export const Container = styled(LinearGradient).attrs(({ theme }) => ({
   colors: theme.COLORS.GRADIENT,
@@ -8,4 +9,14 @@ export const Container = styled(LinearGradient).attrs(({ theme }) => ({
 }))`
   flex: 1;
   justify-content: center;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace() + 48,
+  },
+})`
+  width: 100%;
+  padding: 0 32px;
 `;
