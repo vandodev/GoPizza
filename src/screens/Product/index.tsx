131d4fc8405314b4ddcb9decf500a562a21c800f
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-23 21:32:41 -0300

    :rocket: Component ButtonBack

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ButtonBack } from "@components/ButtonBack";
 import { Platform, TouchableOpacity } from "react-native";
 import { Container, Header, Title, DeletLabel } from "./styles";
 
@@ -6,6 +7,7 @@ export function Product() {
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
+        <ButtonBack />
         <Title>Cadastrar</Title>
         <TouchableOpacity>
           <DeletLabel>Deletar</DeletLabel>
