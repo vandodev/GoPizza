7fb111927654a74eb9bcbf32aa2cce9ab3f6145f
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-27 23:11:32 -0300

    :rocket: Header da home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
-import { Container } from "./style";
+import { TouchableOpacity } from "react-native";
+import { Container, Greeting, GreetingEmoji, GreetingText } from "./style";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components/native";
+
+import { Header } from "../Product/styles";
+import happyEmoji from "@assets/happy.png";
 
 export function Home() {
-  return <Container></Container>;
+  const { COLORS } = useTheme();
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá admin</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
 }
