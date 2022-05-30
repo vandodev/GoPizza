5c0994e93bc0f6fc762ae169a4cdfdb881f3239e
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-29 22:01:30 -0300

    :rocket: Buscando pizzas cadastradas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import { Alert, TouchableOpacity } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 import {
   Container,
   Greeting,
@@ -19,6 +20,35 @@ import { ProductCard, ProductProps } from "@components/ProductCard";
 
 export function Home() {
   const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection("pizzas")
+      .orderBy("name_insensitive")
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        console.log(data);
+      })
+      .catch(() =>
+        Alert.alert("Consulta", "Não foi possível realizar a consulta.")
+      );
+  }
+
+  useEffect(() => {
+    fetchPizzas("");
+  }, []);
+
   return (
     <Container>
       <Header>
