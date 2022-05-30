45d1bd3afd37b2b73d7e10d2d569c2517dab11ad
Evandro oliveira <vandoaparecido@hotmail.com>
2022-05-29 21:41:56 -0300

    :rocket: Componente ProductCard

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -15,6 +15,7 @@ import { useTheme } from "styled-components/native";
 import { Header } from "../Product/styles";
 import { Search } from "@components/Search";
 import happyEmoji from "@assets/happy.png";
+import { ProductCard, ProductProps } from "@components/ProductCard";
 
 export function Home() {
   const { COLORS } = useTheme();
@@ -35,6 +36,15 @@ export function Home() {
         <Title>Cardápio</Title>
         <MenuItemsNumber>10 pizzas</MenuItemsNumber>
       </MenuHeader>
+      <ProductCard
+        data={{
+          id: "1",
+          name: "pizza",
+          description: "pizza de catupiri com tomate",
+          photo_url:
+            "https://media.istockphoto.com/photos/delicious-vegetarian-pizza-on-white-picture-id1192094401?k=20&m=1192094401&s=612x612&w=0&h=jesvXuPyvqM36GQ5QEvJrL3QZjK6YKsziUUF3ZbW0gw=",
+        }}
+      />
     </Container>
   );
 }
