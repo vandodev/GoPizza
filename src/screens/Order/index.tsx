c577fcba23a98a578d20fca51a7e73d352ff7d7b
Evandro oliveira <vandoaparecido@hotmail.com>
2022-06-01 22:46:53 -0300

    :rocket: Criando component RadioButton

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Platform } from "react-native";
-import { Container, Header, Photo } from "./styles";
+import { Container, Header, Photo, Sizes } from "./styles";
 
 import { ButtonBack } from "@components/ButtonBack";
+import { RadioButton } from "@components/RadioButton";
 
 export function Order() {
   return (
@@ -11,6 +12,10 @@ export function Order() {
         <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
       </Header>
       <Photo source={{ uri: "https://github.com/vandodev.png" }} />
+
+      <Sizes>
+        <RadioButton title="Pequeno" selected={false} />
+      </Sizes>
     </Container>
   );
 }
