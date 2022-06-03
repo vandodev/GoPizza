630ae3dc479f3ce24c93bc5250b4ed51f7bf473f
Evandro oliveira <vandoaparecido@hotmail.com>
2022-06-02 21:51:46 -0300

    :rocket: Estrutura e header de pedidos feitos

---
+++ src/screens/Orders/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, Header, Title } from "./styles";
+
+export function Orders() {
+  return (
+    <Container>
+      <Header>
+        <Title>Pedidos feitos</Title>
+      </Header>
+    </Container>
+  );
+}
