fd923b82c43674bf4c454dff71cf247b4e2a51dd
Evandro oliveira <vandoaparecido@hotmail.com>
2022-06-02 21:37:03 -0300

    :rocket: Finalizando formulário de pedidos

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,30 +1,62 @@
 import React, { useState } from "react";
-import { Platform } from "react-native";
-import { Container, Header, Photo, Sizes } from "./styles";
+import { Platform, ScrollView } from "react-native";
+import {
+  Container,
+  ContentScroll,
+  Header,
+  Photo,
+  Sizes,
+  Form,
+  FormRow,
+  InputGroup,
+  Label,
+  Title,
+  Price,
+} from "./styles";
 
 import { ButtonBack } from "@components/ButtonBack";
 import { RadioButton } from "@components/RadioButton";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
 
 export function Order() {
   const [size, setSize] = useState("");
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-      </Header>
-      <Photo source={{ uri: "https://github.com/vandodev.png" }} />
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+        </Header>
+        <Photo source={{ uri: "https://github.com/vandodev.png" }} />
 
-      <Sizes>
-        {PIZZA_TYPES.map((item) => (
-          <RadioButton
-            onPress={() => setSize(item.id)}
-            selected={size === item.id}
-            key={item.id}
-            title={item.name}
-          />
-        ))}
-      </Sizes>
+        <Form>
+          <Title> Nome da pizza </Title>
+          <Label> Selecione um tamanho </Label>
+          <Sizes>
+            {PIZZA_TYPES.map((item) => (
+              <RadioButton
+                onPress={() => setSize(item.id)}
+                selected={size === item.id}
+                key={item.id}
+                title={item.name}
+              />
+            ))}
+          </Sizes>
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+          <Price>Valor de R$ 100000 </Price>
+          <Button title="Confirmar pedido" />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 }
